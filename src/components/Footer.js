26f5653497cd00c8b1db29f02367377106d963f8
stephenonchieku1 <onchiekustephen901@gmail.com>
2023-07-10 01:14:16 +0300

    feat:footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,18 +1,21 @@
 import React from 'react';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
 
 const Footer = () => {
   return (
     <footer className="min-h-[10vh] w-full p-8 flex items-center justify-center">
-      <p className="text-sm">
-        Made with 💖 from{' '}
-        <a
-          href="https://twitter.com/Arshadayvid"
-          rel="noopener noreferrer"
-          target="_blank"
-        >
-          David Asaolu
-        </a>
-      </p>
+     
+      <Box>
+      <Typography color="text.secondary" variant="body2" sx={{ mb: 4 }}>
+        Copyright © {new Date().getFullYear()} SET FREE SOLUTIONS .
+      </Typography>
+      <Typography>
+        Developed by Stephen onchieku
+
+      </Typography>
+      </Box>
+
     </footer>
   );
 };
