9b50aecbde9e9a8b7e105e0f50349965ff408e97
stephenonchieku1 <onchiekustephen901@gmail.com>
2023-07-13 15:53:36 +0300

    feat:footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,52 +1,13 @@
 import React from 'react';
-import Box from '@mui/material/Box';
-import Typography from '@mui/material/Typography';
-import {  FaTwitter, FaInstagram, FaGithubAlt, FaLinkedin } from 'react-icons/fa';
 
 
 const Footer = () => {
-  return (
-    <footer className="min-h-[10vh] w-full p-8 flex items-center justify-center">
-     
-      <Box>
-      <Typography color="text.secondary" variant="body2" sx={{ mb: 4 }}>
-        Copyright © {new Date().getFullYear()} SET FREE SOLUTIONS .
-      </Typography>
-      <Typography>
-        <div>
-         <ul className='footer-social-links flex flex-c '>
-                    <li>
-                        <a href = "https://github.com/stephenonchieku1" className='text-white'>
-                            <FaGithubAlt/>
-                        </a>
-                    </li>
-                    <li>
-                        <a href = "https://twitter.com/StephenOnchieku" className='text-white'>
-                            <FaTwitter />
-                        </a>
-                    </li>
-                    <li>
-                        <a href = "https://www.instagram.com/xtrevasone/" className='text-white'>
-                            <FaInstagram />
-                        </a>
-                    </li>
-                    <li>
-                        <a href = "https://www.linkedin.com/in/stephen-onchieku-70297121b/" className='text-white'>
-                            <FaLinkedin />
-                        </a>
-                    </li>
-                 
-                 
-                </ul>
-                </div> hi
-      </Typography>
-      <Typography>
-        Developed by Stephen onchieku
-
-      </Typography>
-      </Box>
-
-    </footer>
+  return (    
+   <footer className="px-4 divide-y dark:bg-gray-800 dark:text-gray-100 w-full">
+	
+	
+</footer>   
+  
   );
 };
 
