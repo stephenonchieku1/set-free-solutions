34d0e9b2f6a1a8609a3d162b3c8abafc6d17ea0c
stephenonchieku1 <onchiekustephen901@gmail.com>
2023-07-14 18:39:33 +0300

    changed developer to menu

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -45,16 +45,19 @@ const Footer = () => {
 				</ul>
 			</div>
       <div className="space-y-3">
-				<h3 className="uppercase dark:text-gray-50">Developers</h3>
+				<h3 className="uppercase dark:text-gray-50">Menu</h3>
 				<ul className="space-y-1">
-					<li>
-						<a rel="noopener noreferrer" href="#">Public API</a>
+				<li>
+						<a rel="noopener noreferrer" href="/">Home</a>
 					</li>
 					<li>
-						<a rel="noopener noreferrer" href="#">Documentation</a>
+						<a rel="noopener noreferrer" href="/about">About us</a>
 					</li>
 					<li>
-						<a rel="noopener noreferrer" href="#">Guides</a>
+						<a rel="noopener noreferrer" href="/services">Services</a>
+					</li>
+					<li>
+						<a rel="noopener noreferrer" href="/contacts">Contacts</a>
 					</li>
 				</ul>
 			</div>
