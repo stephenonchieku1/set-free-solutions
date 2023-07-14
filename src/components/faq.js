42ff05c3cf6c0c12ed3a852d3ec6c9f658a1343e
stephenonchieku1 <onchiekustephen901@gmail.com>
2023-07-14 18:59:25 +0300

    added starter code

--- src/components/faq.js
+++ src/components/faq.js
@@ -2,7 +2,18 @@ import React from 'react'
 
 function faq() {
   return (
-    <div>faq</div>
+    <div>
+        <section>
+            <div>
+                <p>How it works</p>
+                <h2>Frequently asked questions</h2>
+                <div>
+                    <summary> </summary>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde neque in fugiat magni, quas animi enim veritatis deleniti ex. Impedit.</p>
+                </div>
+            </div>
+        </section>
+    </div>
   )
 }
 
