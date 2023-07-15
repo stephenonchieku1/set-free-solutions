9414b582a0892fcebd3df4a8b1fcab944756bee5
stephenonchieku1 <onchiekustephen901@gmail.com>
2023-07-15 23:36:14 +0300

    added section

--- src/components/Services/Services.js
+++ src/components/Services/Services.js
@@ -2,7 +2,14 @@ import React from 'react'
 
 function Services() {
   return (
-    <div>Services</div>
+        <section className="bg-gray-100 text-gray-800">
+            <div className="container px-6 py-12 mx-auto">
+                <div className="grid items-center gap-4 xl:grid-cols-5">
+                    <div className="max-w-2xl mx-auto my-8 space-y-4 text-center xl:col-span-2 xl:text-left">
+                        <h2 className="text-4xl font-bold">Duo assum utroque appetere an</h2>
+                        <p className="text-gray-600">Pri ex magna scaevola moderatius. Nullam accommodare no vix, est ei diceret alienum, et sit cetero malorum. Et sea iudico consequat, est sanctus adipisci ex.</p>
+                    </div>
+            </section>
   )
 }
 
