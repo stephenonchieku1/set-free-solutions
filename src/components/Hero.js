2f74d92d06c78d790ab0600c943853992878fce2
stephenonchieku1 <onchiekustephen901@gmail.com>
2023-07-21 01:07:20 +0300

    feat:third card

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -43,7 +43,23 @@ const Hero = () => {
                 </div>
               </div>
 
-            
+              <div className="pt-6 w-full md:w-4/12 px-4 text-center">
+                <div className="relative flex flex-col min-w-0 break-words bg-white w-full mb-8 shadow-lg rounded-lg">
+                  <div className="px-4 py-5 flex-auto">
+                    <div className="text-white p-3 text-center inline-flex items-center justify-center w-12 h-12 mb-5 shadow-lg rounded-full bg-green-400">
+                      <i className="fas fa-fingerprint"></i>
+                    </div>
+                    <h6 className="text-xl font-semibold">
+                      Verified Company
+                    </h6>
+                    <p className="mt-2 mb-4 text-gray-600">
+                      Write a few lines about each one. A paragraph describing a feature will be enough. Keep you user engaged!
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            </div>
      </section>
     </div>
   );
