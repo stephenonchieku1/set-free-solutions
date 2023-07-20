096bac694384a76b26b03e9a5910ceff1e7e5dad
stephenonchieku1 <onchiekustephen901@gmail.com>
2023-07-21 01:11:31 +0300

    added section into main

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -4,10 +4,8 @@ import invoice from '../images/invoice.svg';
 
 const Hero = () => {
   return (
-    <div>
-      <main>
-
-      </main>
+    
+      <main>  
      
 
         <section className="pb-20 bg-gray-300 -mt-auto">
@@ -61,7 +59,8 @@ const Hero = () => {
             </div>
             </div>
      </section>
-    </div>
+       </main>
+    
   );
 };
 
