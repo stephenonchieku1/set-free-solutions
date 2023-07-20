8b54051cd0e239cdfc7a5af3e7c0e389793d548f
stephenonchieku1 <onchiekustephen901@gmail.com>
2023-07-20 23:58:04 +0300

    changed bg color

--- src/pages/PageNotFound.js
+++ src/pages/PageNotFound.js
@@ -3,10 +3,10 @@ import { Link } from 'react-router-dom';
 
 const PageNotFound = () => {
   return (
-    <main className="w-full min-h-screen bg-gray-200 flex flex-col items-center justify-center">
+    <main className="w-full min-h-screen bg-gray-800 flex flex-col items-center justify-center">
       <h3 className="text-2xl mb-4 font-bold">Page does not exit 😭</h3>
       <Link to="/" className="text-red-500">
-        Go home ❇️
+        Go home page ❇️
       </Link>
     </main>
   );
