db3c76609ac4d7c94b8df4929eb530b6b7415aab
stephenonchieku1 <onchiekustephen901@gmail.com>
2023-07-21 23:02:33 +0300

    add index to homepage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,7 +4,7 @@ import Hero from '../components/Hero';
 import HomeSection from '../components/HomeSection';
 import Nav from '../components/Nav';
 import Services from "../components/Services/Services";
-import 
+import  Index from "../components/Index"
 
 const Home = () => {
   return (
@@ -14,6 +14,7 @@ const Home = () => {
       <HomeSection />
       <Services/>          
       <Footer />
+      <Index/>
     </div>
   );
 };
