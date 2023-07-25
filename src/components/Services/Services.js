bdaff7b7b214234e156548e9ed402f461613d346
stephenonchieku1 <onchiekustephen901@gmail.com>
2023-07-25 23:18:24 +0300

    updated

--- src/components/Services/Services.js
+++ src/components/Services/Services.js
@@ -12,9 +12,11 @@ function Services() {
               malorum. Et sea iudico consequat, est sanctus adipisci ex.
             </p>
           </div>
+
+
           <div className="p-6 xl:col-span-3">
             <div className="grid gap-4 md:grid-cols-2">
-              <div className="p-6 rounded shadow-md bg-gray-50">
+              <div className="p-6 rounded shadow-2xl  hover:text-white bg-gray-50 hover:bg-sky-500">
                 <p>
                   An audire commodo habemus cum. Ne sed corrumpit repudiandae. Tota aliquip democritum pro in, nec
                   democritum intellegam ne. Propriae volutpat dissentiet ea sit, nec at lorem inani tritani, an ius
@@ -28,8 +30,50 @@ function Services() {
                   </div>
                 </div>
               </div>
+              <div className="p-6 rounded shadow-2xl  hover:text-white hover:bg-sky-500  bg-gray-50">
+                <p>
+                  An audire commodo habemus cum. Ne sed corrumpit repudiandae. Tota aliquip democritum pro in, nec
+                  democritum intellegam ne. Propriae volutpat dissentiet ea sit, nec at lorem inani tritani, an ius
+                  populo perfecto vituperatoribus. Eu cum case modus salutandi, ut eum vocent sensibus reprehendunt.
+                </p>
+                <div className="flex items-center mt-4 space-x-">
+                  <img src="https://source.unsplash.com/50x50/?portrait?1" alt="" className="w-12 h-12 bg-center bg-cover rounded-full bg-gray-500" />
+                  <div>
+                    <p className="text-lg font-semibold">Graphics Design</p>
+                    <p className="text-sm text-gray-600">CTO of Company Co.</p>
+                  </div>
+                </div>
+              </div>
+              <div className="p-6 rounded shadow-2xl  hover:text-white bg-gray-50  hover:bg-sky-500 ">
+                <p>
+                  An audire commodo habemus cum. Ne sed corrumpit repudiandae. Tota aliquip democritum pro in, nec
+                  democritum intellegam ne. Propriae volutpat dissentiet ea sit, nec at lorem inani tritani, an ius
+                  populo perfecto vituperatoribus. Eu cum case modus salutandi, ut eum vocent sensibus reprehendunt.
+                </p>
+                <div className="flex items-center mt-4 space-x-4">
+                  <img src="https://source.unsplash.com/50x50/?portrait?1" alt="" className="w-12 h-12 bg-center bg-cover rounded-full bg-gray-500" />
+                  <div>
+                    <p className="text-lg font-semibold">Web develepment</p>
+                    <p className="text-sm text-gray-600">CTO of Company Co.</p>
+                  </div>
+                </div>
+              </div>
+              <div className="p-6 rounded shadow-2xl hover:text-white bg-gray-50 hover:bg-sky-500">
+                <p>
+                  An audire commodo habemus cum. Ne sed corrumpit repudiandae. Tota aliquip democritum pro in, nec
+                  democritum intellegam ne. Propriae volutpat dissentiet ea sit, nec at lorem inani tritani, an ius
+                  populo perfecto vituperatoribus. Eu cum case modus salutandi, ut eum vocent sensibus reprehendunt.
+                </p>
+                <div className="flex items-center mt-4 space-x-4">
+                  <img src="https://source.unsplash.com/50x50/?portrait?1" alt="" className="w-12 h-12 bg-center bg-cover rounded-full bg-gray-500" />
+                  <div>
+                    <p className="text-lg font-semibold">Mobile App Development </p>
+                    <p className="text-sm text-gray-600">CTO of Company Co.</p>
+                  </div>
+                </div>
+              </div>
               {/* Add the missing closing div tags for the following grid items */}
-              <div className="p-6 rounded shadow-md bg-gray-50">
+              <div className="p-6 rounded shadow-2xl  hover:text-white bg-gray-50 hover:bg-sky-500 ">
                 <p>
                   Sit wisi sapientem ut, pri civibus temporibus voluptatibus et, ius cu hinc fabulas. Nam meliore
                   minimum et, regione convenire cum id. Ex pro eros mucius consectetuer, pro magna nulla nonumy ne, eam
@@ -38,11 +82,18 @@ function Services() {
                 <div className="flex items-center mt-4 space-x-4">
                   <img src="https://source.unsplash.com/50x50/?portrait?2" alt="" className="w-12 h-12 bg-center bg-cover rounded-full bg-gray-500" />
                   <div>
-                    <p className="text-lg font-semibold">WEB DEVELOPMENT</p>
+                    <p className="text-lg font-semibold">Dgital Marketing</p>
                     <p className="text-sm text-gray-600">CTO of Company Co.</p>
                   </div>
                 </div>
               </div>
+              <a href="#" class="group block max-w-xs mx-auto rounded-lg p-6 bg-white ring-1 ring-slate-900/5 shadow-lg space-y-3 hover:bg-sky-500 hover:ring-sky-500">
+                       <div class="flex items-center space-x-3">
+                        <svg class="h-6 w-6 stroke-sky-500 group-hover:stroke-white" fill="none" viewBox="0 0 24 24"></svg>
+                          <h3 class="text-slate-900 group-hover:text-white text-sm font-semibold">New project</h3>
+                        </div>
+                        <p class="text-slate-500 group-hover:text-white text-sm">Create a new project from a variety of starting templates.</p>
+                      </a>
               {/* Closing div tags for the other grid items */}
             </div>
           </div>
