4eea37c845fcd03df04f671e109b73d63c892091
stephenonchieku1 <onchiekustephen901@gmail.com>
2023-07-29 23:10:49 +0300

    updates

--- src/components/About.js
+++ src/components/About.js
@@ -1,14 +1,11 @@
 import React from 'react'
 import Footer from './Footer'
+import HomeSection from './HomeSection'
 
 function About() {
   return (
     <div>
-        <p>
-       
-        Set Free Solutions is a duly registered company whose core business is IT and Branding. Being a solutions driven company, we pride ourselves for helping each and every client find the right solutions to achieve their strategic goals. We offer our clients a completely fresh perspective to Branding and IT solutions.
-        Set Free Solutions draws its wealth of experience from providing exeptional products and services to Government departments, NGOs, Foundations, Corparates and businesses. We offer a wide range of products and services and pride ourselves in providing solutions rapidly and imaginatively.
-        </p>
+        <HomeSection/>
         <Footer/>
     </div>
    
