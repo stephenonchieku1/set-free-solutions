d4ffd7a082e95e11d056f7d3b9d2d782198a00cf
stephenonchieku1 <onchiekustephen901@gmail.com>
2023-08-02 22:29:20 +0300

    created form

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import Footer from './Footer'
 import HomeSection from './HomeSection'
-
+import Progress from "./progress"
 function About() {
   return (
     <div>
         <HomeSection/>
         <Footer/>
+        <Progress/>
     </div>
    
   )
--- src/components/progress.js
+++ src/components/progress.js
@@ -1,24 +1,56 @@
-import React from 'react'
 
-function progress() {
+    import React, { useState } from "react";
+import { Input, Form } from "@chakra-ui/react";
+
+const Progress = () => {
+  const [formData, setFormData] = useState({
+    email: "",
+    name: "",
+    message: "",
+  });
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // Send the form data to the email address stephenonchieku1@gmail.com
+    fetch("mailto:stephenonchieku1@gmail.com", {
+      method: "POST",
+      body: JSON.stringify(formData),
+    });
+  };
+
   return (
-    <div>
-    <div class="progress-wrapper">
-    <div class="progress-info">
-      <div class="progress-percentage">
-        <span class="text-sm font-weight-bold">60%</span>
-      </div>
-    </div>
-    <div class="progress">
-      <div class="progress-bar bg-gradient-dark" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style={{width:'60%'}}></div>
-    </div>
-  </div>
-  
+    <form onSubmit={handleSubmit}>
+      <Input
+        type="email"
+        name="email"
+        placeholder="Email"
+        value={formData.email}
+        onChange={handleInputChange}
+      />
+      <Input
+        type="text"
+        name="name"
+        placeholder="Name"
+        value={formData.name}
+        onChange={handleInputChange}
+      />
+      <Input
+        type="text"
+        name="message"
+        placeholder="Message"
+        value={formData.message}
+        onChange={handleInputChange}
+      />
+      <button type="submit">Submit</button>
+    </form>
+  );
+};
 
-  
-  
-  </div>
-  )
-}
 
-export default progress
+
+export default Progress
